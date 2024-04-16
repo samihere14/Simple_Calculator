3faa9551a9dfd00e5fbfdb96d4a761f40d5b28b9
Muhammad Sami <134046729+samihere14@users.noreply.github.com>
2024-04-16 11:22:26 +0500

    1_multiplication_table.c

--- 01_multiplication_table.c
+++ 01_multiplication_table.c
@@ -2,7 +2,7 @@
 int main(){
     int num;
 // take the number as an input from the user
-printf ("Enter the value of the number whose is to be printed");
+printf ("Enter the number you want the multiplication table");
 scanf ("%d",&num);
 for (int i = 1; i < 11; i++)
 {
